c6d5081074f4a6d4ae37e363aeaecdc8ce0cd606
Anonymous <acharon.hades@gmail.com>
2015-03-31 09:30:47 +0000

    Commit import 3

--- src/main/java/org/ms/explorer/type/Neuron.java
+++ src/main/java/org/ms/explorer/type/Neuron.java
@@ -5,5 +5,45 @@ import java.util.List;
 
 public abstract class Neuron extends ElementBase {
 
+private String name;
+  private List<Synapse> synapses;
   
+  public String getId() {
+    return id;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+    setChangeDate();
+  }
+  
+  public List<Synapse> getSynapses() {
+    return synapses;
+  }
+  
+  public void addSynapse(Synapse synapse) {
+    if (synapses == null) {
+      synapses = new ArrayList<Synapse>();
+    }
+    synapses.add(synapse);
+    setChangeDate();
+  }
+  
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    
+    builder.append(super.toString());
+    builder.append("  name = " + name + "\n");
+    if (synapses != null) {
+      for (Synapse synapse : synapses) {
+        builder.append("  synapse = " + synapse + "\n");
+      }
+    }
+    
+    return builder.toString();
+  }  
 }
