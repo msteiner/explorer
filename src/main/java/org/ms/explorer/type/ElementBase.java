7d008adf1734e33afcca10eb1e53e7c3e2266193
Anonymous <acharon.hades@gmail.com>
2015-03-31 09:10:15 +0000

    commit import 2

---
+++ src/main/java/org/ms/explorer/type/ElementBase.java
@@ -0,0 +1,4 @@
+package org.ms.explorer.type;
+
+public class ElementBase {
+}
---
+++ src/main/java/org/ms/explorer/type/Neuron.java
@@ -0,0 +1,9 @@
+package org.ms.explorer.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Neuron extends ElementBase {
+
+  
+}
