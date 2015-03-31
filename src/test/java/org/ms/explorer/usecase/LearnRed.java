3f41af103422ede1b242f608ab5f8db7f5cb0d59
Anonymous <acharon.hades@gmail.com>
2015-03-31 13:44:11 +0000

    add description

--- src/test/java/org/ms/explorer/usecase/LearnRed.java
+++ src/test/java/org/ms/explorer/usecase/LearnRed.java
@@ -2,6 +2,11 @@ package org.ms.explorer.usecase;
 
 /**
  * The usecase LearnRed is an example to show how a Neuron interacts. Goal of all actions is to learn what "red" is.
+ * 
+ * Test setup: 
+ * We are going to start with nothing. This means a empty repository.
+ * 
+ * 
  */
 public class LearnRed {
 }
